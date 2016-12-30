b42d2365dcf53456521dd591eff9f0ae14fa4902
DigitalRs <malcifra@gmail.com>
2016-12-30 14:08:56 +0500

    update check

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -40,7 +40,7 @@ namespace Checkpoints
 	( 27000,  uint256("0x4996994d049c7c5f0fa940aed5d9c9665825a483b01353164873c3bfecf61d8b") )
 	( 70000,  uint256("0x4d05bb08fec6d0720bc0d334cedc3a56d089a79a880539f4e455b04fdc3a8b5c") )
 	( 88000,  uint256("0xbeeb1c5527e6c47880e456c02673570e5c57253ff689ac8ca990bd5b832bda08") )
-   	( 196750,  uint256("0x0bcff437dc6dce0686c3c717e5dcfa7bc951dd3a26da692202b1ae223f908637") )
+   	( 200320,  uint256("0x580f4c77180493e53c734d530bfba9c02bd721ac8bedf65787341e18fab48135") )
     ;
 
     static MapCheckpoints mapCheckpointsTestnet =
